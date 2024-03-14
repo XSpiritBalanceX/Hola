22b0de04b44505d536eed1faa9165847d9f78d0e
Helen <lenagreidon@mail.ru>
2024-03-14 11:22:07 +0300

    created axios api

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import { useEffect } from "react";
 import { translate } from "@i18n";
-import { useAppSelector } from "@store/hook";
+import { useAppSelector, useAppDispatch } from "@store/hook";
 import * as holaSelectors from "@store/selectors";
 import ScrollToTop from "@components/scrollToTop/ScrollToTop";
 import RouterComponent from "@components/router/RouterComponent";
+import { axiosAPI } from "@axiosApi/axiosAPI";
+import { loginUser } from "@store/holaSlice";
+
+axiosAPI.setGetItem((key) => localStorage.getItem(key));
+axiosAPI.setSetItem((key, value) => localStorage.setItem(key, value));
 
 const App = () => {
   const locale = useAppSelector(holaSelectors.localeSelect);
   const { i18n } = translate();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     i18n.changeLanguage(locale);
     // eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    axiosAPI.setLogout(() =>
+      dispatch(
+        loginUser({
+          isLogin: false,
+          token: "",
+          expiresIn: 0,
+          email: "",
+          refreshToken: "",
+        })
+      )
+    );
+  }, [dispatch]);
 
   return (
     <>
---
+++ src/axiosApi/axiosAPI.ts
@@ -0,0 +1,115 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+export const BASE_URL = "https://212.193.62.231:8000/api/v1";
+export const TOKEN_KEY = "hola_access_token";
+export const REFRESH_TOKEN_KEY = "hola_refresh_token";
+export const TOKEN_EXPIRES_KEY = "tokenExpires";
+export const LOGIN_KEY = "login";
+
+class AxiosAPI {
+  //@ts-ignore
+  private axiosInstance: AxiosInstance;
+
+  logout: () => void = () => {
+    console.error("empty");
+  };
+  setItem: (key: string, value: any) => void = () => {
+    console.error("empty");
+  };
+  public getItem: (key: string) => any = () => {
+    console.error("empty");
+  };
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: BASE_URL,
+    });
+
+    this.axiosInstance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      async (error) => {
+        console.error(error);
+        if (error.response?.status === 401 && !error.config._retry) {
+          error.config._retry = true;
+          try {
+            const newToken = await this.fetchToken();
+            this.setItem(TOKEN_KEY, newToken);
+            this.axiosInstance.defaults.headers.common.Authorization = `Bearer ${newToken}`;
+            error.config.headers.Authorization = `Bearer ${newToken}`;
+            return this.axiosInstance(error.config);
+          } catch (refreshError) {
+            this.logout();
+            return Promise.reject(refreshError);
+          }
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  setLogout(logout: () => void) {
+    this.logout = logout;
+  }
+
+  setSetItem(setItem: (key: string, value: any) => void) {
+    this.setItem = setItem;
+  }
+
+  setGetItem(getItem: (key: string) => any) {
+    this.getItem = getItem;
+  }
+
+  async fetchToken() {
+    const refreshToken = this.getItem(REFRESH_TOKEN_KEY);
+    if (!refreshToken) throw Error("some err");
+
+    const result = await axios.post(`${BASE_URL}/auth/refresh`, {
+      refresh: refreshToken,
+    });
+
+    /* const tokenExpires =
+      new Date().getTime() + result.data.AuthenticationResult.ExpiresIn * 1000; */
+
+    this.setItem(TOKEN_KEY, result.data.access);
+    //this.setItem(TOKEN_EXPIRES_KEY, tokenExpires.toString());
+  }
+
+  getAxiosInstance() {
+    return this.axiosInstance;
+  }
+}
+
+export const axiosAPI = new AxiosAPI();
+
+export const axiosInstance = axiosAPI.getAxiosInstance();
+
+axiosInstance.interceptors.request.use(
+  async function (config) {
+    return new Promise(async (resolve) => {
+      try {
+        const token = axiosAPI.getItem(TOKEN_KEY);
+        const expTime = Number(axiosAPI.getItem(TOKEN_EXPIRES_KEY) ?? "0") ?? 0;
+        const curTime = new Date().getTime();
+        if (expTime && (expTime - curTime <= -3000 || !token)) {
+          await axiosAPI.fetchToken();
+        }
+        const updToken = axiosAPI.getItem(TOKEN_KEY);
+        if (updToken) {
+          // @ts-ignore
+          config.headers = {
+            ...config.headers,
+            authorization: updToken,
+          };
+        }
+        resolve(config);
+      } catch (err) {
+        console.log("axiosInstance.interceptors.request.use", err);
+        axiosAPI.logout();
+        resolve(config);
+      }
+    });
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
