1c1420a40acdd9888990ddabb182305b6c1055fe
Helen <lenagreidon@mail.ru>
2024-05-20 13:56:16 +0300

    added emoji in chat

--- src/components/controllersUserChat/ControllersMessage.tsx
+++ src/components/controllersUserChat/ControllersMessage.tsx
@@ -4,6 +4,8 @@ import { translate } from "@i18n";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import moment from "moment";
 import { IControllersMessageProps } from "./TypesControllersUserChat";
+import EmojiPicker, { EmojiClickData, Categories } from "emoji-picker-react";
+import EmojiIcon from "@components/icons/EmojiIcon";
 import "./ControllersUserChat.scss";
 
 const ControllersMessage = ({
@@ -13,6 +15,7 @@ const ControllersMessage = ({
   const userID = localStorage.getItem("hola_user_id");
 
   const [newMessage, setNewMessage] = useState("");
+  const [isShowEmoji, setIsShowEmoji] = useState(false);
 
   const handleTypeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage(e.currentTarget.value);
@@ -26,17 +29,82 @@ const ControllersMessage = ({
     };
     cbHandleAddMessage(new_message);
     setNewMessage("");
+    setIsShowEmoji(false);
   };
+
+  const handleShowEmoji = () => {
+    setIsShowEmoji(!isShowEmoji);
+  };
+
+  const handleAddEmoji = (emoji: EmojiClickData, event: MouseEvent) => {
+    setNewMessage((prevState) => {
+      return prevState + emoji.emoji;
+    });
+  };
+
+  const customCategoriesEmoji = [
+    {
+      category: Categories.SUGGESTED,
+      name: t("lastUsedEmoji"),
+    },
+    {
+      category: Categories.SMILEYS_PEOPLE,
+      name: t("smileysAndFacesEmoji"),
+    },
+    {
+      category: Categories.ANIMALS_NATURE,
+      name: t("animalAndNatureEmoji"),
+    },
+    {
+      category: Categories.FOOD_DRINK,
+      name: t("foodAndDrinkEmoji"),
+    },
+    {
+      category: Categories.TRAVEL_PLACES,
+      name: t("travelAndPlacesEmoji"),
+    },
+    {
+      category: Categories.ACTIVITIES,
+      name: t("activitiesEmoji"),
+    },
+    {
+      category: Categories.OBJECTS,
+      name: t("objectEmoji"),
+    },
+    {
+      category: Categories.SYMBOLS,
+      name: t("symbolsEmoji"),
+    },
+    {
+      category: Categories.FLAGS,
+      name: t("flagsEmoji"),
+    },
+  ];
 
   return (
     <Box className="controllerBox">
-      <TextField
-        type="search"
-        value={newMessage}
-        onChange={handleTypeMessage}
-        className="messageField"
-        placeholder={t("message")}
-      />
+      <Box className="fieldWithEmojiContainer">
+        <TextField
+          type="search"
+          value={newMessage}
+          onChange={handleTypeMessage}
+          className="messageField"
+          placeholder={t("message")}
+        />
+        <Button type="button" onClick={handleShowEmoji} className="emojiButton">
+          <EmojiIcon fill="#797979" />
+        </Button>
+        {isShowEmoji && (
+          <EmojiPicker
+            className="chatEmojiContainer"
+            onEmojiClick={handleAddEmoji}
+            searchDisabled
+            skinTonesDisabled
+            previewConfig={{ showPreview: false }}
+            categories={customCategoriesEmoji}
+          />
+        )}
+      </Box>
       <Button type="button" onClick={handleSendMessage} className="sendButton">
         <ArrowUpwardIcon />
       </Button>
