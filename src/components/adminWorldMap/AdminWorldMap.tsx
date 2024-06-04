f2635c3a80848273118b1ecf1d8c4dbf1d6ce3f8
Helen <lenagreidon@mail.ru>
2024-06-04 12:19:38 +0300

    added name of part of the world in map

--- src/components/adminWorldMap/AdminWorldMap.tsx
+++ src/components/adminWorldMap/AdminWorldMap.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import { Box } from "@mui/material";
 import WorldMap from "react-world-map";
+import { translate } from "@i18n";
 import "./AdminWorldMap.scss";
 
 const AdminWorldMap = () => {
+  const { t } = translate("translate", { keyPrefix: "adminAnalyticsPage" });
+
+  const namePartOfWorld = [
+    { part: "na", label: "northAmerica" },
+    { part: "sa", label: "southAmerica" },
+    { part: "eu", label: "europe" },
+    { part: "as", label: "asia" },
+    { part: "af", label: "africa" },
+    { part: "oc", label: "australia" },
+  ];
+
   const [selectedPart, setSelectedPart] = useState("eu");
 
   const handleSelectPartOfWorld = (part: string) => {
@@ -11,8 +23,23 @@ const AdminWorldMap = () => {
   };
 
   return (
-    <Box>
-      <WorldMap selected={selectedPart} onSelect={handleSelectPartOfWorld} />
+    <Box className="analyticsByUsersContainer">
+      <Box className="usersByLocationsBox">
+        <p className="titleBox">{t("usersByLocations")}</p>
+      </Box>
+      <Box className="worldMapContainer">
+        {namePartOfWorld.map((el, ind) => (
+          <p
+            key={ind}
+            className={`nameOfPart part_${el.label} ${
+              el.part === selectedPart ? "selectedPart" : ""
+            }`}
+          >
+            {t(el.label)}
+          </p>
+        ))}
+        <WorldMap selected={selectedPart} onSelect={handleSelectPartOfWorld} />
+      </Box>
     </Box>
   );
 };
