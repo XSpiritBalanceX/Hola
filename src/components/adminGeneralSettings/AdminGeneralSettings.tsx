d253e6917af936bb74602f207b66bbc2af346bf0
Helen <lenagreidon@mail.ru>
2024-06-10 10:14:17 +0300

    added part of admin settings

---
+++ src/components/adminGeneralSettings/AdminGeneralSettings.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import "./AdminGeneralSettings.scss";
+
+const AdminGeneralSettings = () => {
+  return <Box>AdminGeneralSettings</Box>;
+};
+
+export default AdminGeneralSettings;
--- src/pages/admin_settings/AdminSettingsPage.tsx
+++ src/pages/admin_settings/AdminSettingsPage.tsx
@@ -1,14 +1,27 @@
 import { Container, Box } from "@mui/material";
 import AdminMenu from "@components/adminMenu/AdminMenu";
 import SettingsLinks from "./SettingsLinks";
+import { useParams } from "react-router-dom";
+import AdminGeneralSettings from "@components/adminGeneralSettings/AdminGeneralSettings";
 import "./AdminSettingsPage.scss";
 
+type TPartsOfSettings = {
+  general: JSX.Element;
+};
+
 const AdminSettingsPage = () => {
+  const { part } = useParams();
+
+  const partsOsSettings: TPartsOfSettings = {
+    general: <AdminGeneralSettings />,
+  };
+
   return (
     <Container className="adminSettingsContainer">
       <AdminMenu />
       <Box className="adminSettingsContent">
         <SettingsLinks />
+        {partsOsSettings[part as keyof TPartsOfSettings]}
       </Box>
     </Container>
   );
